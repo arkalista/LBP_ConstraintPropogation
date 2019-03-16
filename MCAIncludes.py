7344a97a813f6d3fffff51992735b048333f4a35
Alishan-GT <alishan@gaditek.com>
2019-03-16 17:17:52 +0500

    Code Refactored

--- MCAIncludes.py
+++ MCAIncludes.py
@@ -1,13 +1,15 @@
 ######################### MCA - Class ###############
 import queue as queue
 import numpy as np
+from joblib import Parallel, delayed
+import multiprocessing
+import copy
 
 ############# Procedure: HeaviSide : Threshold Function ##########################
 # Input:
 #   h is the difference of the pixel with it's neighbour
 # Output:
 #   Returns the flag for the constraint
-
 def HeaviSide(h):
     if (h == 0):
         return 0
@@ -16,14 +18,11 @@ def HeaviSide(h):
     else:
         return -1
 
-
 ############# Procedure: LBP for a given pixel value ##########################
 # Input:
 #   h is the difference of the pixel with it's neighbour
 # Output:
 #   Returns the flag for the constraint
-
-
 def lbp(h):
     if (h < 0):
         return 0
@@ -42,7 +41,6 @@ def lbp(h):
 #   lbpinfo : p,q,pixel value, lbppattern_list, constraint_list
 
 # Todo's : Create a datastructure for lbpinfo and encode the output in it for further use
-
 def calculateLBP(p, q, NEIGHBORS, cImage):
     h1 = NEIGHBORS[:, 0] + p
     h2 = NEIGHBORS[:, 1] + q
@@ -53,14 +51,12 @@ def calculateLBP(p, q, NEIGHBORS, cImage):
     pixelLbp = int(''.join(str(e) for e in pixelLbp), 2)
     return [p, q, pixel, pixelLbp, pixelDiff]
 
-
 ############# Procedure: Check Padded Pixels in the Image ##########################
 # Input:
 #   NEIGHBORS an array of off set => ([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1]])
 #   NEIGHBORS_OFFSET is an index to NEIGHBORS array, values can be in range 0 to 7
 
 # Output:
-
 def validateNPI(NEIGHBORS_OFFSET, m, n, NEIGHBORS, p, q):
     try:
         x = m + NEIGHBORS[NEIGHBORS_OFFSET][0]
@@ -73,7 +69,6 @@ def validateNPI(NEIGHBORS_OFFSET, m, n, NEIGHBORS, p, q):
         h = 1
     return (-1, -1)
 
-
 ############# Procedure: Creating Dictionary for Plato Pixels ##########################
 # Input:
 #   PixelLBP is a dictionary
@@ -82,7 +77,6 @@ def validateNPI(NEIGHBORS_OFFSET, m, n, NEIGHBORS, p, q):
 #   constraint => {0:Equal, 1:Superior, -1: Inferior}
 # Output:
 #   returns a dictionary {key:(p,q) value:[(xn,yn)]} xn,yn => constraint neighbour of p,q
-
 def getConstNeigh(PixelLBP, NEIGHBORS, p, q, constraint):
     k = 0
     RawNodes = list(PixelLBP)
@@ -95,12 +89,10 @@ def getConstNeigh(PixelLBP, NEIGHBORS, p, q, constraint):
         k = k + 1
     return platoDict
 
-
 ############# Procedure: Check if the plato node is minima ##########################
 # Input:
 # Output:
 # Description:
-
 def isMinimaTree(plat, conMatplateaus):
     for tPoint in plat:
         tConMat = conMatplateaus[tPoint]
@@ -109,12 +101,10 @@ def isMinimaTree(plat, conMatplateaus):
             return 0
     return 1
 
-
 ############# Procedure: Check if the plato node is maxima ##########################
 # Input:
 # Output:
 # Description:
-
 def isMaximaTree(plat, conMatplateaus):
     for tPoint in plat:
         tConMat = conMatplateaus[tPoint]
@@ -123,14 +113,10 @@ def isMaximaTree(plat, conMatplateaus):
             return 0
     return 1
 
-
-
-
 ############# Procedure: load And PreProcess Image ##########################
 # Input:
 # Output:
 # Description:
-
 def LoadImage(image):
     topRow = image[0, :]
     bottomRow = image[-1, :]
@@ -141,12 +127,10 @@ def LoadImage(image):
     row, col = cImage.shape
     return [row, col, cImage]
 
-
 ############# Procedure: Extract Plateuas ##########################
 # Input:
 # Output:
 # Description:
-
 def ExtractPlateau(image, platoDict, conMatplateaus):
     minima = []
     maxima = []
@@ -189,13 +173,10 @@ def ExtractPlateau(image, platoDict, conMatplateaus):
             TreeID = TreeID + 1
     return [platopixel, plateutree, PointIndex, minima, maxima, plateutreeVal]
 
-
-
 ############ Procedure: Generate LBP Image ##########################
 # Input:
 # Output:
 # Description:
-
 def GenerateLBPImage(image,NEIGHBORS):
     iData = LoadImage(image)
     row = iData[0]
@@ -212,7 +193,6 @@ def GenerateLBPImage(image,NEIGHBORS):
 # Input:
 # Output:
 # Description:
-
 def getMinimaNodeID(MinimaRefereces, plateutree, PointIndex, activeNodeID):
     minNodeListID = []
     NodeList = plateutree[activeNodeID]
@@ -226,7 +206,6 @@ def getMinimaNodeID(MinimaRefereces, plateutree, PointIndex, activeNodeID):
 # Input:
 # Output:
 # Description:
-
 def getMaximaNodeID(MaximaRefereces, plateutree, PointIndex, activeNodeID):
     maxNodeListID = []
     NodeList = plateutree[activeNodeID]
@@ -278,12 +257,10 @@ def UpdateTreeDepth_Minima(nodeID, MinimaTree, stepSize =1):
 
     return MinimaTree
 
-
 ############# Procedure: Code for Extrema Extents  ##########################
 # Input:
 # Output:
 # Description:
-
 def expandTree_Minima(minimaIndex, MinimaRefereces, plateutree, PointIndex, stepSize =1):
     activeQueue = queue.Queue()
     passiveQueue = queue.Queue()
@@ -334,12 +311,10 @@ def expandTree_Minima(minimaIndex, MinimaRefereces, plateutree, PointIndex, step
 
     return MinimaTree
 
-
 ############# Procedure: Update SubTree Depth Maxima ##########################
 # Input:
 # Output:
 # Description:
-
 def UpdateTreeDepth_Maxima(nodeID, MaximaTree, stepSize =1):
     activeQueue = queue.Queue()
     passiveQueue = queue.Queue()
@@ -379,7 +354,6 @@ def UpdateTreeDepth_Maxima(nodeID, MaximaTree, stepSize =1):
 # Input:
 # Output:
 # Description:
-
 def expandTree_Maxima(maximaIndex, MaximaRefereces, plateutree, PointIndex):
     activeQueue = queue.Queue()
     passiveQueue = queue.Queue()
@@ -430,13 +404,10 @@ def expandTree_Maxima(maximaIndex, MaximaRefereces, plateutree, PointIndex):
 
     return MaximaTree
 
-
-
 ############# Procedure: Code for Creating tree from Images  ##########################
 # Input:
 # Output:
 # Description:
-
 def CreateImageFromTree_Minima(minForest_Parallel,row,col,plateutree):
         minCellArray = {}
         minLevelArray = {}
@@ -462,7 +433,6 @@ def CreateImageFromTree_Minima(minForest_Parallel,row,col,plateutree):
 
         return [pass_Image,pass_LevelImage]
 
-
 def CreateImageFromTree_Maxima(maxForest_Parallel, row, col, plateutree):
     maxCellArray = {}
     maxLevelArray = {}
@@ -487,3 +457,241 @@ def CreateImageFromTree_Maxima(maxForest_Parallel, row, col, plateutree):
             pass_LevelImage[index] = PixelTreeIndex
 
     return [pass_Image, pass_LevelImage]
+
+def CompareImagesLBP(originalImage, VariantImage):
+    NEIGHBORS = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1]])
+    originalLBP = GenerateLBPImage(originalImage, NEIGHBORS)
+    pass1LBP = GenerateLBPImage(VariantImage, NEIGHBORS)
+
+    if ((originalLBP == pass1LBP).all()):
+        return 1
+    else:
+        return 0
+
+def PreProcess_ExtremaExpansion(image, isMinima):
+    NEIGHBORS = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1]])
+
+    ############ Calculating LBP of the Image ####################
+    # -- Takes a numpy array and append rows and columns, i.e new image is of dimensions [m+2,n+2]
+    iData = LoadImage(image)
+    row = iData[0]
+    col = iData[1]
+    cImage = iData[2]
+    # -- calculates LBP for each of the pixel in the image
+    # -- Following data structure
+    # -- [i,j,pixelval,lbpval,[constraintval]] where constraint value are can be 0 (equal), 1 (greater),
+    # and -1 (smaller)
+    PixelLBP = [calculateLBP(i, j, NEIGHBORS, cImage) for i, j in np.ndindex(cImage.shape) if
+                i > 0 and i < row - 1 and j > 0 and j < col - 1]
+
+    ####################### Boiler Plate for Plateau, Minima, and Maxima Extraction ###############
+    # -- create a data structure to maintain the plateaus in the image
+    # -- key : (i,j) - pixel index
+    # -- value : [(i1,j1),..,(in,jn)] - List of co-ordinates in the image
+    platoDict = getConstNeigh(PixelLBP, NEIGHBORS, row, col, 0)
+    MinimaRefereces = getConstNeigh(PixelLBP, NEIGHBORS, row, col, 1)
+    MaximaRefereces = getConstNeigh(PixelLBP, NEIGHBORS, row, col, -1)
+
+    conMatplateaus = {}
+    for pixel in PixelLBP:
+        conMatplateaus[(pixel[0] - 1, pixel[1] - 1)] = pixel[4]
+
+    ####################### Code for Plateau, Minima, and Maxima Extraction ###############
+    pData = ExtractPlateau(image, platoDict, conMatplateaus)
+    plateutree = pData[1]
+    PointIndex = pData[2]
+    minima = pData[3]
+    maxima = pData[4]
+    plateutreeVal = pData[5]
+
+    if (isMinima):
+        return minima, MinimaRefereces, plateutree, plateutreeVal, PointIndex, row, col
+    else:
+        return maxima, MaximaRefereces, plateutree, plateutreeVal, PointIndex, row, col
+
+def GenerateImageVariations_Minima(image):
+
+    ################ Boiler ###############
+    minima, MinimaRefereces, plateutree, plateutreeVal, PointIndex, row, col = PreProcess_ExtremaExpansion(image,1)
+
+    ####################### Preprocessing Code for for Image Generation _L1 and Minima Tree Expansion ###############
+
+    num_cores = multiprocessing.cpu_count()
+
+    minForest_Parallel = Parallel(n_jobs=-1, backend="multiprocessing", batch_size=num_cores, pre_dispatch='1.5*n_jobs',
+                                  verbose=5) \
+        (delayed(expandTree_Minima)(minima[i], MinimaRefereces, plateutree, PointIndex) for i in range(0, len(minima)))
+
+    #################### Code for Image Generation _L1 #################
+
+    PassImages_L1 = CreateImageFromTree_Minima(minForest_Parallel, row, col, plateutree)
+    pass1_Image_Minima = PassImages_L1[0]
+
+    #################### Preprocessing Image Generation _L2,3,4 #################
+
+    minForest_L2_Parallel = []
+    minForest_L3_Parallel = []
+    minForest_L4_Parallel = []
+    for index, minTree in enumerate(minForest_Parallel):
+        maxDepth = []
+        maxDepthNodeID = []
+        rootNodeID = -1
+        for key in minTree.keys():
+            maxDepth.append(minTree[key][0][1])
+            maxDepthNodeID.append(minTree[key][0][0])
+            if minTree[key][0][0] == 0:
+                rootNodeID = key
+
+        # for rootNodeID go into the original image and fetch the actual value of the local minima
+        # and set it to maxTree[rootNodeID][0][1] and pass it to update tree depth to generate the image, the one
+        # with root node initialzed to actual value of the minima.
+        stepSize = 1
+
+        # We are taking the max value because at the depth of the true will be a value equal to depth Since we
+        # initialize the root with 0 and increment with +1 at each step
+        rootValueFromTreeDepth = max(maxDepth)
+
+        # We take the node ID of the Pixel at the Depth of the tree and get it's subsequent index in the imaege and
+        # use the value from pass 1 images, this will be minima value
+        # influenced from the global constraints
+        treeDepth_Ind = np.argmax(maxDepth)
+        treeDepthNodeID = maxDepthNodeID[treeDepth_Ind]
+        rootValueFromPass1Image = pass1_Image_Minima[plateutree[treeDepthNodeID][0]]
+
+        # we take the root node id and take it's subsequent index in the image and use the value from original image
+        minimaActualValue = plateutreeVal[rootNodeID]
+
+        minTree_L2 = copy.deepcopy(minTree)
+        minTree_L3 = copy.deepcopy(minTree)
+        minTree_L4 = copy.deepcopy(minTree)
+
+        # Initializing the tree root with the Max Depth Value
+        minTree_L2[rootNodeID][0][1] = rootValueFromTreeDepth
+        minTree_L2 = UpdateTreeDepth_Minima(rootNodeID, minTree_L2)
+        minForest_L2_Parallel.append(minTree_L2)
+
+        # Initializing the tree root with the Value Derived from Global Constraints
+        minTree_L3[rootNodeID][0][1] = rootValueFromPass1Image
+        minTree_L3 = UpdateTreeDepth_Minima(rootNodeID, minTree_L3)
+        minForest_L3_Parallel.append(minTree_L3)
+
+        # Initializing the tree root with the Actual Value of Minima
+        minTree_L4[rootNodeID][0][1] = minimaActualValue
+        minTree_L4 = UpdateTreeDepth_Minima(rootNodeID, minTree_L4, stepSize)
+        minForest_L4_Parallel.append(minTree_L4)
+
+    #################### Code for Image Generation _L2,3,4 #################
+
+    PassImages_L2 = CreateImageFromTree_Minima(minForest_L2_Parallel, row, col, plateutree)
+    pass2_Image_Minima = PassImages_L2[0]
+
+    PassImages_L3 = CreateImageFromTree_Minima(minForest_L3_Parallel, row, col, plateutree)
+    pass3_Image_Minima = PassImages_L3[0]
+
+    PassImages_L4 = CreateImageFromTree_Minima(minForest_L4_Parallel, row, col, plateutree)
+    pass4_Image_Minima = PassImages_L4[0]
+
+    #################### Compare if the generated image and the orignal image have same structure #################
+    pass1 = CompareImagesLBP(image, pass1_Image_Minima)
+    pass2 = CompareImagesLBP(image, pass2_Image_Minima)
+    pass3 = CompareImagesLBP(image, pass3_Image_Minima)
+    pass4 = CompareImagesLBP(image, pass4_Image_Minima)
+
+    if (pass1 and pass2 and pass3 and pass4):
+        return [pass1_Image_Minima, pass2_Image_Minima, pass3_Image_Minima, pass4_Image_Minima]
+    else:
+        return []
+
+def GenerateImageVariations_Maxima(image):
+
+    ################ Boiler ###############
+    maxima, MaximaRefereces, plateutree, plateutreeVal, PointIndex, row, col = PreProcess_ExtremaExpansion(image,0)
+
+    ####################### Code for Tree Expansion - Minima ###############
+
+    num_cores = multiprocessing.cpu_count()
+
+    maxForest_Parallel = Parallel(n_jobs=-1, backend="multiprocessing", batch_size=num_cores, pre_dispatch='1.5*n_jobs',
+                                  verbose=5) \
+        (delayed(expandTree_Maxima)(maxima[i], MaximaRefereces, plateutree, PointIndex) for i in range(0, len(maxima)))
+
+    #################### Code for Image Generation _L1 #################
+
+    PassImages_L1_Maxima = CreateImageFromTree_Maxima(maxForest_Parallel, row, col, plateutree)
+    pass1_Image_Maxima = PassImages_L1_Maxima[0]
+
+    ###########  Add Code for Updating the root of the tree #############
+
+    maxForest_L2_Parallel = []
+    maxForest_L3_Parallel = []
+    maxForest_L4_Parallel = []
+    for index, maxTree in enumerate(maxForest_Parallel):
+        maxDepth = []
+        maxDepthNodeID = []
+        rootNodeID = -1
+        for key in maxTree.keys():
+            maxDepth.append(maxTree[key][0][1])
+            maxDepthNodeID.append(maxTree[key][0][0])
+            if maxTree[key][0][0] == 0:
+                rootNodeID = key
+
+        # for rootNodeID go into the original image and fetch the actual value of the local minima
+        # and set it to maxTree[rootNodeID][0][1] and pass it to update tree depth to generate the image, the one
+        # with root node initialzed to actual value of the minima.
+        stepSize = 1
+
+        # We are taking the min value because at the depth of the true will be a value equal to 255 - depth Since we
+        # initialize the root with 255 and decrement with -1 at each step
+        rootValueFromTreeDepth = min(maxDepth)
+
+        # We take the node ID of the Pixel at the Depth of the tree and get it's subsequent index in the image and
+        # use the value from pass 1 images, this will be minima value
+        # influenced from the global constraints
+        treeDepth_Ind = np.argmin(maxDepth)
+        treeDepthNodeID = maxDepthNodeID[treeDepth_Ind]
+        rootValueFromPass1Image = pass1_Image_Maxima[plateutree[treeDepthNodeID][0]]
+
+        # we take the root node id and take it's subsequent index in the image and use the value from original image
+        maximaActualValue = plateutreeVal[rootNodeID]
+
+        maxTree_L2 = copy.deepcopy(maxTree)
+        maxTree_L3 = copy.deepcopy(maxTree)
+        maxTree_L4 = copy.deepcopy(maxTree)
+
+        # Initializing the tree root with the Max Depth Value
+        maxTree_L2[rootNodeID][0][1] = rootValueFromTreeDepth
+        maxTree_L2 = UpdateTreeDepth_Maxima(rootNodeID, maxTree_L2)
+        maxForest_L2_Parallel.append(maxTree_L2)
+
+        # Initializing the tree root with the Value Derived from Global Constraints
+        maxTree_L3[rootNodeID][0][1] = rootValueFromPass1Image
+        maxTree_L3 = UpdateTreeDepth_Maxima(rootNodeID, maxTree_L3)
+        maxForest_L3_Parallel.append(maxTree_L3)
+
+        # Initializing the tree root with the Actual Value of Maxima
+        maxTree_L4[rootNodeID][0][1] = maximaActualValue
+        maxTree_L4 = UpdateTreeDepth_Maxima(rootNodeID, maxTree_L4, stepSize)
+        maxForest_L4_Parallel.append(maxTree_L4)
+
+    #################### Code for Image Generation _L2 #################
+
+    PassImages_L2 = CreateImageFromTree_Maxima(maxForest_L2_Parallel, row, col, plateutree)
+    pass2_Image_Maxima = PassImages_L2[0]
+
+    PassImages_L3 = CreateImageFromTree_Maxima(maxForest_L3_Parallel, row, col, plateutree)
+    pass3_Image_Maxima = PassImages_L3[0]
+
+    PassImages_L4 = CreateImageFromTree_Maxima(maxForest_L4_Parallel, row, col, plateutree)
+    pass4_Image_Maxima = PassImages_L4[0]
+
+    #################### Compare if the generated image and the orignal image have same structure #################
+    pass1 = CompareImagesLBP(image, pass1_Image_Maxima)
+    pass2 = CompareImagesLBP(image, pass2_Image_Maxima)
+    pass3 = CompareImagesLBP(image, pass3_Image_Maxima)
+    pass4 = CompareImagesLBP(image, pass4_Image_Maxima)
+
+    if (pass1 and pass2 and pass3 and pass4):
+        return [pass1_Image_Maxima, pass2_Image_Maxima, pass3_Image_Maxima, pass4_Image_Maxima]
+    else:
+        return []
+
--- MinimaVariations.py
+++ MinimaVariations.py
@@ -1,247 +1,37 @@
+########## Required for Load Image and Display ###########
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.misc
 import matplotlib as mpl
 
-########## Required for Clean Printing ###########
-from pprint import pprint
 
-########## Required for Parallel Processing ###########
+########## Required for Generating Variations ###########
 
 from MCAIncludes import *
-from joblib import Parallel, delayed
-import multiprocessing
-import time
-
-import copy
 
 
-########## Require for Tree Expansion ###################
+# Test Image
+#image = np.array([[20, 27, 24, 26], [16, 23, 30, 32], [22, 22, 20, 19], [22, 10, 35, 19]])
+#print(image.shape)
 
-def GenerateImageVariations_Minima(image):
-    ######### Global Variables ###########################
-
-    NEIGHBORS = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1]])
-
-    # Test Image
-
-    # image = misc.face(gray=True)
-    # image = misc.imresize(image,(64,64))
-    TotalParallelExecutionTime = 0
-
-    # image= np.array([[20,27,24,26],[16,23,30,32],[22,22,20,19],[22,10,35,19]])
-    # print(type(image))
-    # print(image.shape)
-
-    ############ Calculating LBP of the Image ####################
-    start_time = time.time()
-    # -- Takes a numpy array and append rows and columns, i.e new image is of dimensions [m+2,n+2]
-    iData = LoadImage(image)
-    row = iData[0]
-    col = iData[1]
-    cImage = iData[2]
-    print(cImage.shape)
-    # -- calculates LBP for each of the pixel in the image
-    # -- Following data structure
-    # -- [i,j,pixelval,lbpval,[constraintval]] where constraint value are can be 0 (equal), 1 (greater), and -1 (smaller)
-    PixelLBP = [calculateLBP(i, j, NEIGHBORS, cImage) for i, j in np.ndindex(cImage.shape) if
-                i > 0 and i < row - 1 and j > 0 and j < col - 1]
-    # print("For Serial Execution,LBP Procedure took--- %s seconds ---" % (time.time() - start_time))
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-    pprint(PixelLBP)
-
-    ####################### Boiler Plate for Plateau, Minima, and Maxima Extraction ###############
-    start_time = time.time()
-    # -- create a data structure to maintain the plateaus in the image
-    # -- key : (i,j) - pixel index
-    # -- value : [(i1,j1),..,(in,jn)] - List of co-ordinates in the image
-    platoDict = getConstNeigh(PixelLBP, NEIGHBORS, row, col, 0)
-    MinimaRefereces = getConstNeigh(PixelLBP, NEIGHBORS, row, col, 1)
-    MaximaRefereces = getConstNeigh(PixelLBP, NEIGHBORS, row, col, -1)
-    pprint(platoDict)
-    pprint(MinimaRefereces)
-    pprint(MaximaRefereces)
-
-    conMatplateaus = {}
-    for pixel in PixelLBP:
-        conMatplateaus[(pixel[0] - 1, pixel[1] - 1)] = pixel[4]
-    pprint(conMatplateaus)
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-
-    ####################### Code for Plateau, Minima, and Maxima Extraction ###############
-    start_time = time.time()
-    pData = ExtractPlateau(image, platoDict, conMatplateaus)
-    plateuPixel = pData[0]
-    plateutree = pData[1]
-    PointIndex = pData[2]
-    minima = pData[3]
-    maxima = pData[4]
-    plateutreeVal = pData[5]
-
-    pprint(plateuPixel)
-    pprint(plateutree)
-    pprint(PointIndex)
-    pprint(minima)
-    pprint(maxima)
-    pprint(plateutreeVal)
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-
-    ####################### Code for Tree Expansion - Minima ###############
-
-    num_cores = multiprocessing.cpu_count()
-
-    # print(num_cores, " cores are availble for execution")
-
-    start_time = time.time()
-    minForest_Parallel = Parallel(n_jobs=-1, backend="multiprocessing", batch_size=num_cores, pre_dispatch='1.5*n_jobs',
-                                  verbose=5) \
-        (delayed(expandTree_Minima)(minima[i], MinimaRefereces, plateutree, PointIndex) for i in range(0, len(minima)))
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-    pprint(minForest_Parallel)
-
-    #################### Code for Image Generation _L1 #################
-    start_time = time.time()
-
-    PassImages_L1 = CreateImageFromTree_Minima(minForest_Parallel, row, col, plateutree)
-    pass1_Image_Minima = PassImages_L1[0]
-    pass1_LevelImage_Minima = PassImages_L1[1]
-
-    pprint(pass1_Image_Minima)
-    pprint(pass1_LevelImage_Minima)
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-
-    ###########  Add Code for Updating the root of the tree #############
-    ## After max assignmet
-    ## for each tree determine the max value and tree number
-    ## take difference of max value with depth and update tree root.
-    ## Fill the tree with the new root value and update the tree.
-    minForest_L2_Parallel = []
-    minForest_L3_Parallel = []
-    minForest_L4_Parallel = []
-    for index, minTree in enumerate(minForest_Parallel):
-        maxDepth = []
-        maxDepthNodeID = []
-        rootNodeID = -1
-        for key in minTree.keys():
-            maxDepth.append(minTree[key][0][1])
-            maxDepthNodeID.append(minTree[key][0][0])
-            if minTree[key][0][0] == 0:
-                rootNodeID = key
-
-        # for rootNodeID go into the original image and fetch the actual value of the local minima
-        # and set it to maxTree[rootNodeID][0][1] and pass it to update tree depth to generate the image, the one
-        # with root node initialzed to actual value of the minima.
-        pprint(minTree)
-        pprint(rootNodeID)
-        stepSize = 1
-
-        # We are taking the max value because at the depth of the true will be a value equal to depth Since we initialize the root with 0 and increment with +1 at each step
-        rootValueFromTreeDepth = max(maxDepth)
-
-        # We take the node ID of the Pixel at the Depth of the tree and get it's subsequent index in the imaege and use the value from pass 1 images, this will be minima value
-        # influenced from the global constraints
-        treeDepth_Ind = np.argmax(maxDepth)
-        treeDepthNodeID = maxDepthNodeID[treeDepth_Ind]
-        rootValueFromPass1Image = pass1_Image_Minima[plateutree[treeDepthNodeID][0]]
-
-        # we take the root node id and take it's subsequent index in the image and use the value from original image
-        minimaActualValue = plateutreeVal[rootNodeID]
-
-
-        minTree_L2 = copy.deepcopy(minTree)
-        minTree_L3 = copy.deepcopy(minTree)
-        minTree_L4 = copy.deepcopy(minTree)
-
-        # Initializing the tree root with the Max Depth Value
-        minTree_L2[rootNodeID][0][1] = rootValueFromTreeDepth
-        minTree_L2 = UpdateTreeDepth_Minima(rootNodeID, minTree_L2)
-        minForest_L2_Parallel.append(minTree_L2)
-
-        # Initializing the tree root with the Value Derived from Global Constraints
-        minTree_L3[rootNodeID][0][1] = rootValueFromPass1Image
-        minTree_L3 = UpdateTreeDepth_Minima(rootNodeID, minTree_L3)
-        minForest_L3_Parallel.append(minTree_L3)
-
-        # Initializing the tree root with the Actual Value of Minima
-        minTree_L4[rootNodeID][0][1] = minimaActualValue
-        minTree_L4 = UpdateTreeDepth_Minima(rootNodeID, minTree_L4, stepSize)
-        minForest_L4_Parallel.append(minTree_L4)
-
-    #################### Code for Image Generation _L2 #################
-    start_time = time.time()
-
-    PassImages_L2 = CreateImageFromTree_Minima(minForest_L2_Parallel, row, col, plateutree)
-    pass2_Image_Minima = PassImages_L2[0]
-    pass2_LevelImage_Minima = PassImages_L2[1]
-
-    pprint(pass2_Image_Minima)
-    pprint(pass2_LevelImage_Minima)
-
-    PassImages_L3 = CreateImageFromTree_Minima(minForest_L3_Parallel, row, col, plateutree)
-    pass3_Image_Minima = PassImages_L3[0]
-    pass3_LevelImage_Minima = PassImages_L3[1]
-
-    pprint(pass3_Image_Minima)
-    pprint(pass3_LevelImage_Minima)
-
-    PassImages_L4 = CreateImageFromTree_Minima(minForest_L4_Parallel, row, col, plateutree)
-    pass4_Image_Minima = PassImages_L4[0]
-    pass4_LevelImage_Minima = PassImages_L4[1]
-
-    pprint(pass4_Image_Minima)
-    pprint(pass4_LevelImage_Minima)
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + ((time.time() - start_time))
-
-    #################### Compare if the generated image and the orignal image are same - L1 #################
-    start_time = time.time()
-
-    originalLBP = GenerateLBPImage(image, NEIGHBORS)
-    pass1LBP = GenerateLBPImage(pass1_Image_Minima, NEIGHBORS)
-
-    if ((originalLBP == pass1LBP).all()):
-        print("Success L1 generated")
-    else:
-        print("Failure L1 generated")
-
-    TotalParallelExecutionTime = TotalParallelExecutionTime + (time.time() - start_time)
-
-    #################### Compare if the generated image and the orignal image are same - L2 #################
-    start_time = time.time()
-
-    originalLBP = GenerateLBPImage(image, NEIGHBORS)
-    pass2LBP = GenerateLBPImage(pass2_Image_Minima, NEIGHBORS)
-
-    if ((originalLBP == pass2LBP).all()):
-        print("Success L2 generated")
-    else:
-        print("Failure L2 generated")
-
-
-    return [pass1_Image_Minima, pass2_Image_Minima, pass3_Image_Minima, pass4_Image_Minima]
-
-
-
-image = np.array([[20, 27, 24, 26], [16, 23, 30, 32], [22, 22, 20, 19], [22, 10, 35, 19]])
-print(type(image))
-print(image.shape)
-print(image)
-
+# Load Image from Current Directory
 lena = scipy.misc.imread('lena.png', mode='F')
+
+# Resize
 image = scipy.misc.imresize(lena, (64, 64))
 
+# Call Genrate Variation from MCAIncludes
 ImgLst = GenerateImageVariations_Minima(image)
+pass1_Image_Minima = ImgLst[0]
+pass2_Image_Minima = ImgLst[1]
+pass3_Image_Minima = ImgLst[2]
+pass4_Image_Minima = ImgLst[3]
 
-pass1_Image_Minima=ImgLst[0]
-pass2_Image_Minima=ImgLst[1]
-pass3_Image_Minima=ImgLst[2]
-pass4_Image_Minima=ImgLst[3]
 
-imageLabel = 1
+
+# Create a visualization
+imageLabel = "Lena - The CV Girl"
 
 print("Image Label is : " + str(imageLabel))
 plt.figure(1)
@@ -271,6 +61,5 @@ ax = plt.subplot(235)
 ax.set_title("Minima Initialized with Priors", fontsize=7)
 ax.set_axis_off()
 ax.imshow(pass4_Image_Minima, cmap=mpl.cm.gray)
-
 
 plt.show()

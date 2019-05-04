e08e09feb43569de8e3731477879609ecb5f63b6
m-alishan <alishan.merchant@gmail.com>
2019-05-04 15:06:52 +0500

    Added Code for Command Line Argument and Output Variations Code

--- src/Generator.py
+++ src/Generator.py
@@ -1,155 +1,110 @@
-########## Required for Load Image and Display ###########
-
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.misc
-import matplotlib as mpl
 import argparse
+import logging
+########## Required for Load Image and Display ###########
+import imageio as imio
+import skimage as imsk
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 ########## Required for Generating Variations ###########
-
 from MCAIncludes import *
 
-# Test Image
-# image = np.array([[20, 27, 24, 26], [16, 23, 30, 32], [22, 22, 20, 19], [22, 10, 35, 19]])
-# print(image.shape)
 
-# Load Image from Current Directory
+def generateImageVariations(img):
+    imgVar = {}
+    # Test Image
+    # image = np.array([[20, 27, 24, 26], [16, 23, 30, 32], [22, 22, 20, 19], [22, 10, 35, 19]])
+    # print(image.shape)
+    # Resize
+    imgVar['image'] = imsk.transform.resize(img, (64, 64))
 
-# Resize
-image = scipy.misc.imresize(lena, (64, 64))
+    # Call Genrate Variation from MCAIncludes
+    ImgLst = GenerateImageVariations_Minima(imgVar['image'])
+    imgVar['pass1_Image_Minima'] = ImgLst[0]
+    imgVar['pass2_Image_Minima'] = ImgLst[1]
+    imgVar['pass3_Image_Minima'] = ImgLst[2]
+    imgVar['pass4_Image_Minima'] = ImgLst[3]
 
-# Call Genrate Variation from MCAIncludes
-ImgLst = GenerateImageVariations_Minima(image)
-pass1_Image_Minima = ImgLst[0]
-pass2_Image_Minima = ImgLst[1]
-pass3_Image_Minima = ImgLst[2]
-pass4_Image_Minima = ImgLst[3]
+    ImgLst = GenerateImageVariations_Maxima(imgVar['image'])
+    imgVar['pass1_Image_Maxima'] = ImgLst[0]
+    imgVar['pass2_Image_Maxima'] = ImgLst[1]
+    imgVar['pass3_Image_Maxima'] = ImgLst[2]
+    imgVar['pass4_Image_Maxima'] = ImgLst[3]
 
-ImgLst = GenerateImageVariations_Maxima(image)
-pass1_Image_Maxima = ImgLst[0]
-pass2_Image_Maxima = ImgLst[1]
-pass3_Image_Maxima = ImgLst[2]
-pass4_Image_Maxima = ImgLst[3]
+    imgVar['min_avg'] = ((imgVar['pass1_Image_Minima'] + imgVar['pass2_Image_Minima'] + imgVar['pass3_Image_Minima'] +
+                          imgVar['pass4_Image_Minima']) / 4)
+    imgVar['max_avg'] = ((imgVar['pass1_Image_Maxima'] + imgVar['pass2_Image_Maxima'] + imgVar['pass3_Image_Maxima'] +
+                          imgVar['pass4_Image_Maxima']) / 4)
 
-min_avg = ((pass1_Image_Minima + pass2_Image_Minima + pass3_Image_Minima + pass4_Image_Minima) / 4)
-max_avg = ((pass1_Image_Maxima + pass2_Image_Maxima + pass3_Image_Maxima + pass4_Image_Maxima) / 4)
+    imgVar['pass1_image_avg'] = ((imgVar['pass1_Image_Minima'] + imgVar['pass1_Image_Maxima']) / 2)
+    imgVar['pass2_image_avg'] = ((imgVar['pass2_Image_Minima'] + imgVar['pass2_Image_Maxima']) / 2)
+    imgVar['pass3_image_avg'] = ((imgVar['pass3_Image_Minima'] + imgVar['pass3_Image_Maxima']) / 2)
+    imgVar['pass4_image_avg'] = ((imgVar['pass4_Image_Minima'] + imgVar['pass4_Image_Maxima']) / 2)
 
-pass1_image_avg = ((pass1_Image_Minima + pass1_Image_Maxima) / 2)
-pass2_image_avg = ((pass2_Image_Minima + pass2_Image_Maxima) / 2)
-pass3_image_avg = ((pass3_Image_Minima + pass3_Image_Maxima) / 2)
-pass4_image_avg = ((pass4_Image_Minima + pass4_Image_Maxima) / 2)
+    imgVar['min_max_avg'] = ((imgVar['min_avg'] + imgVar['max_avg']) / 2)
 
-min_max_avg = ((min_avg + max_avg) / 2)
+    return imgVar
 
-plt.figure(1)
-plt.title("Hello World - MCA")
 
-ax = plt.subplot(361)
-ax.set_title("Original Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(image, cmap=mpl.cm.gray)
+def plot(_plt, narr, arr, p, q, r):
+    ax = _plt.subplot(p, q, r)
+    ax.set_title(narr, fontsize=5)
+    ax.set_axis_off()
+    ax.imshow(arr, cmap=mpl.cm.gray)
+    return _plt
 
-ax = plt.subplot(362)
-ax.set_title("Pass 1 Minima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass1_Image_Minima, cmap=mpl.cm.gray)
 
-ax = plt.subplot(363)
-ax.set_title("Pass 2 Minima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass2_Image_Minima, cmap=mpl.cm.gray)
+def plotImgVar(plt, imname, imgVar, type='all'):
+    imgVarType = ['image', 'pass1_Image_Minima', 'pass2_Image_Minima', 'pass3_Image_Minima', 'pass4_Image_Minima',
+                  'min_avg', 'pass1_Image_Maxima', 'pass2_Image_Maxima', 'pass3_Image_Maxima', 'pass4_Image_Maxima',
+                  'max_avg', 'pass1_image_avg', 'pass2_image_avg', 'pass3_image_avg', 'pass4_image_avg', 'min_max_avg'
+                  ]
 
-ax = plt.subplot(364)
-ax.set_title("Pass 3 Minima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass3_Image_Minima, cmap=mpl.cm.gray)
+    plt.figure(1)
+    plt.title("Image Varations")
 
-ax = plt.subplot(365)
-ax.set_title("Pass 4 Minima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass4_Image_Minima, cmap=mpl.cm.gray)
+    column = 6
 
-ax = plt.subplot(366)
-ax.set_title("Pass 1,2,3,4 Minima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(min_avg, cmap=mpl.cm.gray)
+    if type == 'all':
 
-ax = plt.subplot(367)
-ax.set_title("Original Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(image, cmap=mpl.cm.gray)
+        row = 3
+        elements = 16
 
-ax = plt.subplot(368)
-ax.set_title("Pass 1 Maxima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass1_Image_Maxima, cmap=mpl.cm.gray)
+        for i in range(0, elements):
+            plt = plot(plt, imgVarType[i], imgVar[imgVarType[i]], row, column, i + 1)
+        plt.savefig(imname + '_all.png', bbox_inches='tight')
 
-ax = plt.subplot(369)
-ax.set_title("Pass 2 Maxima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass2_Image_Maxima, cmap=mpl.cm.gray)
+    elif type == 'minima':
+        row = 1
+        elements = 5
+        plt = plot(plt, imgVarType[0], imgVar[imgVarType[0]], row, column, 1)
+        for i in range(0, elements):
+            plt = plot(plt, imgVarType[i + 1], imgVar[imgVarType[i + 1]], row, column, i + 2)
+        plt.savefig(imname + '_minima.png', bbox_inches='tight')
 
-ax = plt.subplot(3, 6, 10)
-ax.set_title("Pass 3 Maxima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass3_Image_Maxima, cmap=mpl.cm.gray)
+    else:
+        row = 1
+        elements = 5
+        plt = plot(plt, imgVarType[0], imgVar[imgVarType[0]], row, column, 1)
+        for i in range(0, elements):
+            plt = plot(plt, imgVarType[i + 6], imgVar[imgVarType[i + 6]], row, column, i + 2)
+        plt.savefig(imname + '_maxima.png', bbox_inches='tight')
 
-ax = plt.subplot(3, 6, 11)
-ax.set_title("Pass 4 Maxima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass4_Image_Maxima, cmap=mpl.cm.gray)
+    return plt
 
-ax = plt.subplot(3, 6, 12)
-ax.set_title("Pass 1,2,3,4 Maxima Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(max_avg, cmap=mpl.cm.gray)
 
-ax = plt.subplot(3, 6, 13)
-ax.set_title("Original Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(image, cmap=mpl.cm.gray)
-
-ax = plt.subplot(3, 6, 14)
-ax.set_title("Pass 1 Min and Max Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass1_image_avg, cmap=mpl.cm.gray)
-
-ax = plt.subplot(3, 6, 15)
-ax.set_title("Pass 2 Min and Max Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass2_image_avg, cmap=mpl.cm.gray)
-
-ax = plt.subplot(3, 6, 16)
-ax.set_title("Pass 3 Min and Max Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass3_image_avg, cmap=mpl.cm.gray)
-
-ax = plt.subplot(3, 6, 17)
-ax.set_title("Pass 4 Min and Max Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(pass4_image_avg, cmap=mpl.cm.gray)
-
-ax = plt.subplot(3, 6, 18)
-ax.set_title("Pass 1,2,3,4 Min and Max Image", fontsize=7)
-ax.set_axis_off()
-ax.imshow(min_max_avg, cmap=mpl.cm.gray)
-
-plt.show()
-
-# for directory : '../assets/lena.png'
-
-def loadImage(dir):
-
+def loadImage(dir='../assets/lena.png'):
     image = []
     try:
-        image = scipy.misc.imread(dir, mode='F')
+        image = imio.imread(dir, pilmode='F')
     except Exception as e:
-        print("ERROR - "+str(e))
+        logging.error(str(e))
+        exit()
 
     return image
 
-def parse_cmd_line_args_and_run():
+
+def parse_cmd_line_args_and_run(plt):
     parser = argparse.ArgumentParser()
     parser.add_argument("--type",
                         help="The type of augmentation that needs to be generated , can take up following values : "
@@ -160,21 +115,32 @@ def parse_cmd_line_args_and_run():
 
     # check if the input image exists.
     if args.input:
-        image = loadImage(args.input)
+        base_dir = '../assets/'
+        img_dir_in = base_dir + args.input
+        img_dir_out = base_dir + args.input.split('.')[0]
+        image = loadImage(img_dir_in)
     else:
-        print("Houston we have a problem !!! - Wrong input directory for the image")
+        logging.error('Houston we have a problem !!! - Need image name as input')
+        exit()
 
     # check if user wants to generate all variations
 
+    imgVar = generateImageVariations(image)
+
     if args.type == 'all':
-        print("call all")
+        plt = plotImgVar(plt, img_dir_out, imgVar, 'all')
+        return plt
     elif args.type == 'minima':
-        print("call minima")
+        plt = plotImgVar(plt, img_dir_out, imgVar, 'minima')
+        return plt
     elif args.type == 'maxima':
-        print("call maxima")
+        plt = plotImgVar(plt, img_dir_out, imgVar, 'maxima')
+        return plt
     else:
-        print("Houston we have a problem !!! - Wrong Input for the type of augmentation selected")
+        logging.error("Houston we have a problem !!! - Wrong Input for the type of augmentation selected")
+        exit()
 
 
 if __name__ == "__main__":
-    parse_cmd_line_args_and_run()
+    plt = parse_cmd_line_args_and_run(plt)
+    plt.show()
